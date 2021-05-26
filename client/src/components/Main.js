d1c463c1e0bcd5df680dc13a8d00394fa34bb2de
Oliver Vazquez <olivervazquez@gmail.com>
2021-05-26 18:47:53 -0400

    Working for static cities

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,53 +3,106 @@ import { Typography } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Input from '@material-ui/core/Input';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Main = () => {
 
   const [backgroundState, setBackgroundState] = useState({
     color: 'white',
     userInput: '',
-    userInputInvalid: false
+    userInputInvalid: false,
   }) 
 
-  const makeBlue = () => {
-    setBackgroundState({...backgroundState, color: 'blue'})
+  const [weatherState, setWeatherState] = useState({
+    loaded: false,
+    city: null,
+    weatherData: null,
+    temperature: null,
+    feelsLike: null,
+    humidity: null,
+    windSpeed: null,
+    cloudiness: null,
+    rainVolume: null,
+    snowVolume: null,
+  })
+
+  const kelvinToFahrenheit = (temp) => {
+    return (temp - 273.15) * (9/5) + 32
   }
-  const makeRed = () => {
-    setBackgroundState({...backgroundState, color: 'red'})
+
+  const kelvinToCelsius = (temp) => {
+    return (temp - 273.15)
   }
-  const makeYellow = () => {
-    setBackgroundState({...backgroundState, color: 'yellow'})
+
+  const parseWeatherData = () => {
+    console.log(weatherState.weatherData)
+    const city = weatherState.weatherData.name
+    const temperatureK = weatherState.weatherData.main.temp
+    const temperatureF = kelvinToFahrenheit(temperatureK)
+    const temperatureC = kelvinToCelsius(temperatureK)
+    const feelsLikeK = weatherState.weatherData.main.feels_like
+    const feelsLikeF = kelvinToFahrenheit(feelsLikeK)
+    const feelsLikeC = kelvinToCelsius(feelsLikeK)
+    const humidity = weatherState.weatherData.main.humidity
+    const windSpeed = weatherState.weatherData.wind.speed
+    const cloudiness = weatherState.weatherData.clouds.all
+    // const rainVolume = weatherState.weatherData.rain.1h
+    // const snowVolume = weatherState.weatherData.rain.1h
+
+    setWeatherState({...weatherState, 
+      loaded: true,
+      city: city,
+      temperatureK: temperatureK,
+      temperatureF: temperatureF,
+      temperatureC: temperatureC,
+      feelsLikeK: feelsLikeK,
+      feelsLikeF: feelsLikeF,
+      feelsLikeC: feelsLikeC,
+      humidity: humidity,
+      windSpeed: windSpeed,
+      cloudiness: cloudiness,
+      rainVolume: null,
+      snowVolume: null,
+    })
   }
-  const makePurple= () => {
-    setBackgroundState({...backgroundState, color: 'purple'})
+
+  useEffect(() => {
+    console.log('useEffect')
+    if (weatherState.weatherData !== null) {
+      parseWeatherData()
+    }
+  }, [weatherState.weatherData])
+
+  const fetchWeather = async (city) => {
+
+    const API_KEY = process.env.REACT_APP_OPEN_WEATHER_MAP_KEY
+    const address = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+
+    const response = await fetch(address)
+    const weatherData = await response.json()
+    setWeatherState({...weatherState, weatherData: weatherData})
   }
-  const handleChange = (prop) => (event) => {
-    setBackgroundState({...backgroundState, userInput: event.target.value})
-  }
-  const keyPress = (event) => {
-    if(event.keyCode == 13){
-      const s = new Option().style;
-      s.color = backgroundState.userInput;
-      if (s.color !== '') {
-        setBackgroundState({...backgroundState, color: backgroundState.userInput, userInput:'', userInputInvalid: false})
-      }
-      else {
-        setBackgroundState({...backgroundState, userInputInvalid: true})
-      }
-   }
-  }
+
+  const weatherDisplay = <div>
+    <h1>Weather for {weatherState.city}</h1>
+    <h1>{weatherState.temperatureF}°F</h1>
+    <h2>Feels Like {weatherState.feelsLikeF}°F</h2>
+    <h2>{weatherState.humidity}% Humidity</h2>
+    <h2>{weatherState.windSpeed}m/s Winds</h2>
+    <h2>{weatherState.cloudiness}% Cloud Coverage</h2>
+  </div>
 
   return (
     <>
       <CssBaseline />
       <Container maxWidth='xl' disableGutters={true}>
         <Typography component="div" style={{ backgroundColor: backgroundState.color, height: '100vh' }}>
-          <Button variant='outlined' color={backgroundState.color === 'black' ? 'primary' : 'default'} onClick={makeBlue}>blue</Button>
-          <Button variant='outlined' color={backgroundState.color === 'black' ? 'primary' : 'default'} onClick={makeRed}>red</Button>
-          <Button variant='outlined' color={backgroundState.color === 'black' ? 'primary' : 'default'} onClick={makeYellow}>yellow</Button>
-          <Input id="filled-basic" label="Filled" variant="filled" onKeyDown={keyPress} onChange={handleChange('color')} error={backgroundState.userInputInvalid}/>
+          <Button variant='outlined' onClick={() => fetchWeather('boston')}>Display Weather Boston</Button>
+          <Button variant='outlined' onClick={() => fetchWeather('winnipeg')}>Display Weather Boston</Button>
+          <Button variant='outlined' onClick={() => fetchWeather('manhattan')}>Display Weather Boston</Button>
+        {weatherState.loaded &&
+          weatherDisplay
+        }
         </Typography>
       </Container>
     </>
